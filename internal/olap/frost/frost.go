77695479d45a9bc28a7b2bfda9c314e657505758
Megakuul <linus.moser@megakuul.ch>
2026-05-29 23:53:12 +0200

    feat: start implementing logging

--- internal/olap/frost/frost.go
+++ internal/olap/frost/frost.go
@@ -2,49 +2,67 @@ package frost
 
 import (
 	"context"
-	"log/slog"
 	"os"
-	"time"
 
 	"github.com/apache/arrow-go/v18/arrow/memory"
 	"github.com/go-kit/log"
 	"github.com/polarsignals/frostdb"
+	"github.com/polarsignals/frostdb/query"
 	"github.com/thanos-io/objstore/providers/s3"
 )
 
-type Log struct {
-	Timestamp time.Time
-	Level     slog.Level
-	Message   string
-	Labels    map[string]string `frostdb:",asc"`
+type Storage struct {
+	store    *frostdb.ColumnStore
+	database *frostdb.DB
 }
 
-type Storage struct{}
-
-func NewStorage(url, bucket, region, accessKey, secretKey string) (*Storage, error) {
-	bucket, err := s3.NewBucketWithConfig(log.NewJSONLogger(os.Stdout), s3.Config{
-		Bucket:    "my-frostdb-bucket",
-		Endpoint:  "s3.amazonaws.com",
-		Region:    "us-east-1",
-		AccessKey: "",
-		SecretKey: "",
-	}, "frostdb")
+func NewStorage(ctx context.Context, cfg s3.Config, name string) (*Storage, error) {
+	bucket, err := s3.NewBucketWithConfig(log.NewJSONLogger(os.Stdout), cfg, name)
 	if err != nil {
 		return nil, err
 	}
 	store, err := frostdb.New(
 		frostdb.WithReadOnlyStorage(frostdb.NewDefaultObjstoreBucket(bucket)),
 	)
-	defer store.Close()
-	database, err := store.DB(context.TODO(), "cloudjam")
+	database, err := store.DB(ctx, name)
 	if err != nil {
 		return nil, err
 	}
-	logs, err := frostdb.NewGenericTable[Log](database, "log", memory.DefaultAllocator)
-	if err != nil {
-		return nil, err
-	}
-	logs.Write(context.Background(), Log{})
+	return &Storage{
+		store:    store,
+		database: database,
+	}, nil
 
 	// query.NewEngine(memory.DefaultAllocator, )
+}
+
+func (s *Storage) Close() error {
+	return s.store.Close()
+}
+
+type Table[T any] struct {
+	name   string
+	table  *frostdb.GenericTable[T]
+	engine *query.LocalEngine
+}
+
+func NewTable[T any](storage *Storage, name string) (*Table[T], error) {
+	table, err := frostdb.NewGenericTable[T](storage.database, name, memory.DefaultAllocator)
+	if err != nil {
+		return nil, err
+	}
+	return &Table[T]{
+		name:   name,
+		table:  table,
+		engine: query.NewEngine(memory.DefaultAllocator, storage.database.TableProvider()),
+	}, nil
+}
+
+func (t *Table[T]) Insert(ctx context.Context, record T) error {
+	_, err := t.table.Write(context.Background(), record)
+	return err
+}
+
+func (t *Table[T]) Query() query.Builder {
+	return t.engine.ScanTable(t.name)
 }
