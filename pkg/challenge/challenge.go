993aca9735ed4949c4087c5746d21974b1386fe9
Megakuul <linus.moser@megakuul.ch>
2026-07-30 09:20:15 +0200

    feat: complete initial version of wasm api

--- pkg/challenge/challenge.go
+++ pkg/challenge/challenge.go
@@ -1,30 +1,65 @@
 package challenge
 
-const InitCall = "init"
+const Init = "init"
 
 type InitInput struct {
-	Title       string `json:"title,omitempty"`
-	Description string `json:"description,omitempty"`
+	Title       string             `json:"title,omitempty"`
+	Description string             `json:"description,omitempty"`
+	Clues       map[string]float64 `json:"clues,omitempty"`
 }
 
-const RegisterResourceCall = "register_resource"
+const ReadScore = "read_score"
 
-type CreateResource struct {
+type ReadScoreInput struct{}
+
+type ReadScoreOutput struct {
+	Score float64 `json:"score,omitempty"`
+}
+
+const UpdateScore = "update_score"
+
+type UpdateScoreInput struct {
+	Reason    string  `json:"reason,omitempty"`
+	Increment float64 `json:"increme,omitempty"`
+}
+
+type UpdateScoreOutput struct{}
+
+const CreateResource = "create_resource"
+
+type CreateResourceInput struct {
 	Type    string `json:"type,omitempty"`
 	Desired string `json:"desired,omitempty"`
 }
 
-type ReadResource struct {
-	Type    string `json:"type,omitempty"`
-	Desired string `json:"desired,omitempty"`
+type CreateResourceOutput struct{}
+
+const ReadResource = "register_resource"
+
+type ReadResourceInput struct {
+	Type       string `json:"type,omitempty"`
+	Identifier string `json:"identifier,omitempty"`
 }
 
-type UpdateResource struct {
-	Type    string `json:"type,omitempty"`
-	Desired string `json:"desired,omitempty"`
+type ReadResourceOutput struct {
+	State string `json:"state,omitempty"`
 }
 
-type DeleteResource struct {
-	Type    string `json:"type,omitempty"`
-	Desired string `json:"desired,omitempty"`
+const UpdateResource = "update_resource"
+
+type UpdateResourceInput struct {
+	Type       string `json:"type,omitempty"`
+	Identifier string `json:"identifier,omitempty"`
+	Patch      string `json:"patch,omitempty"`
 }
+
+type UpdateResourceOutput struct{}
+
+const DeleteResource = "delete_resource"
+
+type DeleteResourceInput struct {
+	Type       string `json:"type,omitempty"`
+	Identifier string `json:"identifier,omitempty"`
+}
+
+type DeleteResourceOutput struct{}
