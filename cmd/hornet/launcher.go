a7a579d8a214549eb48e1f0bb317ccd292d329fb
Megakuul <linus.moser@megakuul.ch>
2026-08-12 23:01:50 +0200

    feat: integrate new APIs to hornet

--- cmd/hornet/launcher.go
+++ cmd/hornet/launcher.go
@@ -12,20 +12,35 @@ import (
 
 	authmiddleware "codeberg.org/megakuul/cloudjam/internal/auth"
 	"codeberg.org/megakuul/cloudjam/internal/bootstrap"
+	challengerunner "codeberg.org/megakuul/cloudjam/internal/challenge"
 	"codeberg.org/megakuul/cloudjam/internal/olap"
 	"codeberg.org/megakuul/cloudjam/internal/olap/middleware"
+	"codeberg.org/megakuul/cloudjam/internal/provider/cache"
 	"codeberg.org/megakuul/cloudjam/internal/rbac"
+	"codeberg.org/megakuul/cloudjam/internal/scheduler"
 	rbacsvc "codeberg.org/megakuul/cloudjam/internal/server/v1/admin/rbac"
 	"codeberg.org/megakuul/cloudjam/internal/server/v1/admin/role"
 	"codeberg.org/megakuul/cloudjam/internal/server/v1/admin/system"
 	"codeberg.org/megakuul/cloudjam/internal/server/v1/admin/user"
 	"codeberg.org/megakuul/cloudjam/internal/server/v1/auth"
+	"codeberg.org/megakuul/cloudjam/internal/server/v1/cloud/account"
+	"codeberg.org/megakuul/cloudjam/internal/server/v1/cloud/definition"
+	"codeberg.org/megakuul/cloudjam/internal/server/v1/cloud/provider"
+	"codeberg.org/megakuul/cloudjam/internal/server/v1/play/challenge"
+	"codeberg.org/megakuul/cloudjam/internal/server/v1/play/game"
+	"codeberg.org/megakuul/cloudjam/internal/server/v1/play/team"
 	"codeberg.org/megakuul/cloudjam/internal/token"
 	"codeberg.org/megakuul/cloudjam/pkg/api/v1/admin/rbac/rbacconnect"
 	"codeberg.org/megakuul/cloudjam/pkg/api/v1/admin/role/roleconnect"
 	"codeberg.org/megakuul/cloudjam/pkg/api/v1/admin/system/systemconnect"
 	"codeberg.org/megakuul/cloudjam/pkg/api/v1/admin/user/userconnect"
 	"codeberg.org/megakuul/cloudjam/pkg/api/v1/auth/authconnect"
+	"codeberg.org/megakuul/cloudjam/pkg/api/v1/cloud/account/accountconnect"
+	"codeberg.org/megakuul/cloudjam/pkg/api/v1/cloud/definition/definitionconnect"
+	"codeberg.org/megakuul/cloudjam/pkg/api/v1/cloud/provider/providerconnect"
+	"codeberg.org/megakuul/cloudjam/pkg/api/v1/play/challenge/challengeconnect"
+	"codeberg.org/megakuul/cloudjam/pkg/api/v1/play/game/gameconnect"
+	"codeberg.org/megakuul/cloudjam/pkg/api/v1/play/team/teamconnect"
 	"codeberg.org/megakuul/cloudjam/web"
 	"connectrpc.com/connect"
 	"connectrpc.com/validate"
@@ -102,6 +117,18 @@ func Start(ctx context.Context, opts *Options) error {
 		slog.Info(fmt.Sprintf("admin user (%s) registration code: '%s'", opts.AdminEmail, code))
 	}
 
+	providerCache := cache.New(oltpBucket)
+	pluginCache := challengerunner.NewCache(time.Hour * 24)
+	defer pluginCache.Close(ctx)
+
+	runGroups, runCtx := errgroup.WithContext(ctx)
+
+	scheduler := scheduler.New(runCtx)
+	runGroups.Go(func() error {
+		scheduler.Wait()
+		return nil
+	})
+
 	apiMux := http.NewServeMux()
 	authorizer := rbac.New(oltpBucket, opts.PolicyCacheTimeout)
 	apiMux.Handle(authconnect.NewAuthServiceHandler(auth.New(slog.With("system", "svc.auth"), oltpBucket, issuer),
@@ -138,6 +165,50 @@ func Start(ctx context.Context, opts *Options) error {
 			validate.NewInterceptor(),
 		),
 	))
+	apiMux.Handle(providerconnect.NewProviderServiceHandler(provider.New(slog.With("system", "svc.cloud.provider"), scheduler, providerCache, oltpBucket),
+		connect.WithInterceptors(
+			middleware.NewRequestTracer(slog.With("system", "olap.request"), requestIngestor),
+			authmiddleware.New(issuer, authorizer),
+			validate.NewInterceptor(),
+		),
+	))
+	apiMux.Handle(accountconnect.NewAccountServiceHandler(account.New(slog.With("system", "svc.cloud.account"), scheduler, providerCache, oltpBucket),
+		connect.WithInterceptors(
+			middleware.NewRequestTracer(slog.With("system", "olap.request"), requestIngestor),
+			authmiddleware.New(issuer, authorizer),
+			validate.NewInterceptor(),
+		),
+	))
+	apiMux.Handle(definitionconnect.NewDefinitionServiceHandler(definition.New(slog.With("system", "svc.cloud.definition"), oltpBucket),
+		connect.WithInterceptors(
+			middleware.NewRequestTracer(slog.With("system", "olap.request"), requestIngestor),
+			authmiddleware.New(issuer, authorizer),
+			validate.NewInterceptor(),
+		),
+	))
+	apiMux.Handle(gameconnect.NewGameServiceHandler(game.New(slog.With("system", "svc.cloud.game"), oltpBucket),
+		connect.WithInterceptors(
+			middleware.NewRequestTracer(slog.With("system", "olap.request"), requestIngestor),
+			authmiddleware.New(issuer, authorizer),
+			validate.NewInterceptor(),
+		),
+	))
+	apiMux.Handle(teamconnect.NewTeamServiceHandler(team.New(slog.With("system", "svc.cloud.team"), oltpBucket),
+		connect.WithInterceptors(
+			middleware.NewRequestTracer(slog.With("system", "olap.request"), requestIngestor),
+			authmiddleware.New(issuer, authorizer),
+			validate.NewInterceptor(),
+		),
+	))
+	apiMux.Handle(challengeconnect.NewChallengeServiceHandler(challenge.New(slog.With("system", "svc.cloud.challenge"),
+		scheduler, providerCache, pluginCache, oltpBucket, olapBucket,
+	),
+		connect.WithInterceptors(
+			middleware.NewRequestTracer(slog.With("system", "olap.request"), requestIngestor),
+			authmiddleware.New(issuer, authorizer),
+			validate.NewInterceptor(),
+		),
+	))
 	mux.Handle("/api/", http.StripPrefix("/api", apiMux))
 
 	server := http.Server{
@@ -145,8 +216,6 @@ func Start(ctx context.Context, opts *Options) error {
 		Handler:  mux,
 		ErrorLog: slog.NewLogLogger(slog.With("system", "http.server").Handler(), slog.LevelWarn),
 	}
-
-	runGroups, runCtx := errgroup.WithContext(ctx)
 
 	requestCompactor := lake.NewCompactor[olap.Request](olapBucket)
 	logCompactor := lake.NewCompactor[olap.Log](olapBucket)
